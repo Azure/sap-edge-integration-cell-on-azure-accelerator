7e2bf55bcc4af8873116eb8bc18835df0f4f1342
Sebastian Meyer <sebastian.meyer@quibiq.de>
2024-08-15 11:14:55 +0200

    Updates for Quickstart Terraform

--- quickstart/aks/main.tf
+++ quickstart/aks/main.tf
@@ -7,17 +7,19 @@ resource "azurerm_kubernetes_cluster" "quickstart" {
   name                = "k8s-${var.resource_group_name}"
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
-  dns_prefix          = "k8s-${var.resource_group_name}"
-  kubernetes_version  = "1.27.9" 
+  kubernetes_version  = "1.28.10" 
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D4ds_V5"
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    load_balancer_sku = "standard"
   }
 
   sku_tier = "Free"
-  
   identity {
     type = "SystemAssigned"
   }
